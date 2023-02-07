9e430c7f76f42b16cc0c4c44c4503bbb7cba6917
Braian Jassan <braianjassan@gmail.com>
2023-02-07 00:13:06 -0300

    layout, header and footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import FavouritesPage from './pages/FavouritesPage';
+import ParentContext from './context/ParentContext';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Layout from './components/common/Layout';
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
-    </div>
+    <>
+      <ParentContext>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/favourites' element={<FavouritesPage />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </ParentContext>
+    </>
   );
 }
 
