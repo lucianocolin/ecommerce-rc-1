85b46ddaf6b7fa7cd05ee306454c83ac6408bb0a
Braian Jassan <braianjassan@gmail.com>
2023-02-08 19:04:53 -0300

    first attempt to fix main branch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@
 import './App.css';
 import AdminProducts from './pages/AdminProducts';
 import Home from './pages/Home';
-import FavouritesPage from './pages/FavouritesPage';
-import ParentContext from './context/ParentContext';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Layout from './components/common/Layout';
 
 function App() {
   return (
