87186c50fc574644872121b255c680cc1abdf376
Braian Jassan <braianjassan@gmail.com>
2023-02-08 18:23:43 -0300

    removed offcanvas from header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,16 +5,11 @@ import { Container, NavDropdown } from 'react-bootstrap';
 import '../../css/header.css'
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faGlobe, faCartPlus } from "@fortawesome/free-solid-svg-icons";
-import OffCanvas from './OffCanvas';
+import {  faGlobe, faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import NavbarSearch from './NavbarSearch';
 
 const Header = () => {
     const navigate = useNavigate();
-    const [showOffCanvas, setShowOffCanvas] = useState(false);
-
-    const handleClose = () => setShowOffCanvas(false);
-    const handleShow = () => setShowOffCanvas(true);
 
     const goToPage = page => {
         navigate(page);
@@ -25,16 +19,6 @@ const Header = () => {
         <>
             <Navbar className='header' >
                 <Container>
-                    {/* <Button
-                    variant='dark'
-                    className='ms-3 me-4'
-                    onClick={handleShow}
-                >
-                    <FontAwesomeIcon icon={faBars} />
-                </Button>
-
-                <OffCanvas showOffCanvas={showOffCanvas} handleClose={handleClose} /> */}
-
                     <Navbar.Brand>
                         <Nav.Link onClick={() => goToPage('/')}>
                             <FontAwesomeIcon icon={faGlobe} />
