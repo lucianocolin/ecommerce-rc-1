657432c7a092ded8d21da9eef853a5431f8dad8f
lucianocolin <lucianocolin99@gmail.com>
2023-02-21 17:01:14 -0300

    commit to pull main branch from upstream

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import selesion1 from "../imgs/selesion1.jpg";
-import selesion2 from "../imgs/selesion2.jpg";
-import selesion3 from "../imgs/selesion3.jpg";
 import "../css/Home.css";
+import AdminProductsContext from "../context/admin/products/AdminProductsContext";
 
 const Home = () => {
+
+  const { products, getProducts } = useContext(AdminProductsContext)
+
+  useEffect(()=>{
+    getProducts()
+  }, []);
+
   return (
     <>
       <div className="home-slider-section">
         <h1 className="home-offer-title">OFERTAS</h1>
         <Carousel className="home-offers-slider">
-          <Carousel.Item className="home-offers-slider-item">
-            <img
-              className="home-offers-slider-img"
-              src={selesion1}
-              alt="First slide"
-            />
-            <img
-              className="home-offers-slider-img"
+          {
+            products.map((product, index)=>(
+              <Carousel.Item className="home-offers-slider-item">
+                <div className="home-offers-slider-img">
+                  <p className="home-offers-slider-perc">20% OFF</p>
+                  <img
+                    src={selesion1}
+                    alt="First slide 1"
+                  />
+                  <p className="home-offers-slider-description">{product.name}</p>
+                  <p className="home-offers-slider-price">{'$' + product.price}</p>
+                </div>
+                </Carousel.Item>
+            ))
+          }
+            {/* <div className="home-offers-slider-img">
+              <p className="home-offers-slider-perc">20% OFF</p>
+              <img
               src={selesion2}
-              alt="Second slide"
-            />
-            <img
-              className="home-offers-slider-img"
+              alt="First slide 2"
+              />
+              <p className="home-offers-slider-description">Campera aa aa aaa </p>
+              <p className="home-offers-slider-price">$2000</p>
+              </div>
+              <div className="home-offers-slider-img">
+              <p className="home-offers-slider-perc">20% OFF</p>
+              <img
               src={selesion3}
-              alt="Third slide"
-            />
-          </Carousel.Item>
-          {/* <Carousel.Item className="home-offers-slider-item">
-          </Carousel.Item>
-          <Carousel.Item className="home-offers-slider-item">
-          </Carousel.Item> */}
-        </Carousel>
+              alt="First slide 3"
+              />
+              <p className="home-offers-slider-description">Campera aa aa aaa </p>
+              <p className="home-offers-slider-price">$2000</p>
+            </div> */}
+          </Carousel>
       </div>
     </>
   );
